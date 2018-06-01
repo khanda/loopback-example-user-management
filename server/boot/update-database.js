df605d9e93f76170946e36cc7d8eea7d3dfbd616
quyendoan <doanquangquyen@gmail.com>
2018-06-01 23:07:06 +0700

    init commit

---
+++ server/boot/update-database.js
@@ -0,0 +1,29 @@
+/**
+ * Created by quyen on 31/05/2018.
+ */
+/**
+ * Created by quyen on 31/05/2018.
+ */
+'use strict';
+
+module.exports = function (app) {
+  var mysqlDataSource = app.dataSources.mysqlDs;
+  var notInMysqlDsModels = ['Email'];
+  var allModels = app.models();
+  var allModelsName = [];
+  allModels.forEach(function (Model) {
+    allModelsName.push(Model.modelName);
+  });
+  var mySqlModels = allModelsName.filter(checkNotInMySqlDs);
+  mysqlDataSource.autoupdate(mySqlModels, function (err, result) {
+    if (err) {
+      throw err;
+    }
+    console.log('All tables [' + mySqlModels + '] updated in ', mysqlDataSource.adapter.name);
+    // mysqlDataSource.disconnect();
+  });
+
+  function checkNotInMySqlDs(model) {
+    return notInMysqlDsModels.indexOf(model) < 0;
+  }
+};
